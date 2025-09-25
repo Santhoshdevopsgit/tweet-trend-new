407f5f18d74d0d6d6901f45b96a8b93f7edb4d7b
Your Name <you@example.com>
2025-09-25 16:59:16 +0530

    adding V2 version

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-
- resource "aws_instance" "demo-server" {
-  ami           = "ami-02d26659fd82cf299"
-  instance_type = "t2.micro"
-  key_name       = "ddp"
- }
