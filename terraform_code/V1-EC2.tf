9c1210db536be81faa384ff879ffe1c3b6b5dd3c
Your Name <you@example.com>
2025-09-25 17:06:42 +0530

    addign v1 & v2

---
+++ terraform_code/V1-EC2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+ resource "aws_instance" "demo-server" {
+  ami           = "ami-02d26659fd82cf299"
+  instance_type = "t2.micro"
+  key_name       = "ddp"
+ }
